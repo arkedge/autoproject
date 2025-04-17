23a11171a926960c2a850e4fd9ac34ce3bb22ade
yasuda-kyota <dasoran@arkedgespace.com>
2025-04-17 15:52:45 +0900

    remove no-use import

--- src/sentry.server.ts
+++ src/sentry.server.ts
@@ -1,8 +1,6 @@
 import * as Sentry from "@sentry/node";
 import { z } from "zod";
-import log4js from "log4js";
 
-const logger = log4js.getLogger("general");
 const envSchema = z.object({
   SENTRY_DSN: z.string({ required_error: "SENTRY_DSN is required" }),
 });
